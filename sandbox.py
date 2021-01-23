5380ba0179a47918a37301cbf9c633e1130451fb
vijay <elderpatha@gmail.com>
2021-01-23 10:24:12 -0800

    tracer and update

--- sandbox.py
+++ sandbox.py
@@ -3,13 +3,18 @@
 # TODO: 3. Make the Snake Move
 # TODO: 4. In TODO 3, Snake is moving three segments,
 #  so need a 1 seg Snake
+# TODO: 5. Turn the snake
 
 
 from turtle import Screen, Turtle
+import time
+
 screen = Screen()
 screen.title("Snake Game")
 screen.bgcolor("black")
 screen.setup(600, 600)
+screen.tracer(0)  # TODO 4 The tracer() function turns automatic screen updates on or off -- on by default
+# -- and also sets the update() delay
 
 # start_positions = [(-200, 0), (-200, 100), (-200, -100)]
 
@@ -28,8 +33,17 @@ game_is_on = True
 
 # TODO 3
 while game_is_on:
-    for seg in combine_segments:
-        seg.forward(20)
+    time.sleep(0.1)
+    screen.update()  # TODO 4 Update the screen only when segments have moved forward
+    # for seg in combine_segments:
+    #     seg.forward(20)
 
+# TODO 5
+    for seg_num in range(len(combine_segments)-1, 0, -1):
+        new_x = combine_segments[seg_num - 1].xcor()
+        new_y = combine_segments[seg_num - 1].ycor()
+        combine_segments[seg_num].goto(new_x, new_y)
+    combine_segments[0].forward(20)
+    combine_segments[0].left(90)
 
 screen.exitonclick()
